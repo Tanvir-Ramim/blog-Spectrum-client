67f026b2072cd35e9a6b24db74cfd1bce10f5191
Tanvir Ramim <ramimhossan14@gmail.com>
2023-11-08 03:49:26 +0600

    env local file add

--- src/authentication/firebase.config.js
+++ src/authentication/firebase.config.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCWfZwO5w5hXf7VZlGmta5Ebk32KDBWbRA",
-  authDomain: "blog-spectrum.firebaseapp.com",
-  projectId: "blog-spectrum",
-  storageBucket: "blog-spectrum.appspot.com",
-  messagingSenderId: "970011546449",
-  appId: "1:970011546449:web:3cf07e197287625813b970"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
