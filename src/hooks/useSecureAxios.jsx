d7cfafb85f06945f46d22b2f69c4cc6e87c7eecd
Tanvir Ramim <ramimhossan14@gmail.com>
2023-11-08 16:22:08 +0600

    server deploy url add

--- src/hooks/useSecureAxios.jsx
+++ src/hooks/useSecureAxios.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { AuthContext } from '../authentication/AuthProvider';
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://blog-spectrum-server.vercel.app',
     withCredentials: true
 });
 const useSecureAxios = () => {
