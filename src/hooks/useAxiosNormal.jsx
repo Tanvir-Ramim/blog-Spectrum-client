4d260af4e0f974f6d5bdbff1380ac2eeeab4cadb
Tanvir Ramim <ramimhossan14@gmail.com>
2023-11-08 14:08:44 +0600

    git hub related problem

--- src/hooks/useAxiosNormal.jsx
+++ src/hooks/useAxiosNormal.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosNormal=axios.create({
-      baseURL:'https://blog-spectrum-server.vercel.app'
+      baseURL:'https://blog-spectrum-server-6enytn5ke-tanvir-ramim-831d6510.vercel.app'
 })
 
 const useAxiosNormal = () => {
